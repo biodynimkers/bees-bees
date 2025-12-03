3687b20bc563b8e69c7f93771751e1e138c9dc32
ozewieze <stefballyn@hotmail.com>
2025-12-03 21:26:09 +0100

    add new basic folder structure next to the old one

--- src/components/forms/NewApiaryForm.tsx
+++ src/components/forms/NewApiaryForm.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 
 interface NewApiaryFormProps {
-  userId: string;
+  userId: string | undefined;
   onSuccess?: () => void;
 }
 
