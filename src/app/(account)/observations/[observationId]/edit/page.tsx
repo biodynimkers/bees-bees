2b04fc405bffacc0d220313255673912a3baccc3
yamushka <joanjode@student.arteveldehs.be>
2025-12-18 14:28:10 +0100

    Refactor forms for improved structure and styling; enhance error handling and input accessibility

--- src/app/(account)/observations/[observationId]/edit/page.tsx
+++ src/app/(account)/observations/[observationId]/edit/page.tsx
@@ -1,25 +1,33 @@
 import ObservationForm from '@/components/forms/ObservationForm';
+import prisma from '@/lib/client';
+import { redirect } from 'next/navigation';
 
 export default async function EditObservationPage({
   params,
-  searchParams,
 }: {
   params: Promise<{ observationId: string }>;
-  searchParams: Promise<{ hiveId?: string }>;
 }) {
   const { observationId } = await params;
-  const { hiveId } = await searchParams;
 
-  if (!hiveId) {
-    throw new Error('hiveId is required');
+  const observation = await prisma.observation.findUnique({
+    where: { id: parseInt(observationId) },
+    include: { hive: true },
+  });
+
+  if (!observation) {
+    redirect('/observations');
   }
+
   return (
     <div>
       <h1 className="title" style={{ marginTop: '6rem' }}>
         Observatie Bewerken
       </h1>
 
-      <ObservationForm initialObservation={observationId} hiveId={hiveId} />
+      <ObservationForm 
+        initialObservation={observationId} 
+        hiveId={observation.hiveId.toString()} 
+      />
     </div>
   );
 }
